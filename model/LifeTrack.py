0acceee1aab1fc1c3c5f8641325bf1499e22c4d2
Daniel Krawciw <daniel.krawciw@gmail.com>
2025-07-05 19:19:45 -0600

    Combined the functionality with google calendars and emailing to email a rough version of a schedule

--- model/LifeTrack.py
+++ model/LifeTrack.py
@@ -1,7 +1,29 @@
+from datetime import datetime
+from Event import Event
+from access_google_calendar import get_event_components
+from send_email import send_email
+
 class LifeTrack:
     """
     Singleton class that ties events, bank account, and school together and is able to write the email and send it eventually
     """
 
     def __init__(self):
-        pass
+        self.events = []
+    
+    def update_google_calendar_events(self) -> None:
+        self.events += get_event_components()
+
+    def get_text(self) -> str:
+        output_text = ""
+
+        for event in self.events:
+            output_text += f"{str(event)}\n"
+        
+        return output_text
+
+    def send_daily_email(self):
+        self.update_google_calendar_events()
+        date_today = datetime.now().strftime("%m/%d/%Y")
+
+        send_email(subject=f"Daily Email {date_today}", contents=self.get_text())
